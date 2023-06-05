9f3a69384da907205ca46683045a3d18a82cca27
ChiragSingh0980 <chiragsingh9725@gmail.com>
2023-06-05 13:14:15 +0530

    first commit

---
+++ datatype.java
@@ -0,0 +1,11 @@
+public class datatype {
+    public static void main(String[] args) {
+        int a=5;
+        float b=5.7F;
+        char c='c';
+        System.out.println("integer is=" +a);
+        System.out.println("double is="+b);
+        System.out.println("char is="+c);
+    }
+    
+}
