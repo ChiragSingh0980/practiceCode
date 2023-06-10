c756bc582a347d2c4573d353b5e7c9e1d23b346e
ChiragSingh0980 <chiragsingh9725@gmail.com>
2023-06-10 13:14:15 +0530

    first commit

---
+++ kunal.java
@@ -0,0 +1,91 @@
+public class kunal {
+    // static int logic(int x, int y){
+    // int z;
+    // if(x>y){
+    // z = x+y;
+    // }
+    // else {
+    // z = (x +y) * 5;
+    // }
+    // x = 566;
+    // return z;
+    // }
+
+    // public static void main(String[] args) {
+    // int a = 5;
+    // int b = 7;
+    // int c;
+    // Method invocation using Object creation
+    // cwh_31_methods obj = new cwh_31_methods();
+    // c = obj.logic(a, b);
+    // c = logic(a, b);
+    // System.out.println(a + " "+ b);
+    // int a1 = 2;
+    // int b1 = 1;
+    // int c1;
+    // c1 = logic(a1, b1);
+    // System.out.println(c);
+    // System.out.println(c1);
+    // }
+    // public static void main(String[] args) {
+
+    /*
+     * float [] marks = {98.5f, 45.5f, 79.5f, 99.5f, 80.5f};
+     * String [] students ={"Harry", "Rohan", "Shubham", "Lovish"};
+     * System.out.println(students.length);
+     * System.out.println(students[2]);
+     */
+
+    // int[] marks = { 98, 45, 79, 99, 80 };
+    // System.out.println(marks.length);
+
+    // Displaying the Array (Naive way)
+    // System.out.println("Printing using Naive way");
+    // System.out.println(marks[0]);
+    // System.out.println(marks[1]);
+    // System.out.println(marks[2]);
+    // System.out.println(marks[3]);
+    // System.out.println(marks[4]);
+
+    // Displaying the Array (for loop)
+    // System.out.println("Printing using for loop");
+    // for (int i = 0; i < marks.length; i++) {
+    // System.out.println(marks[i]);
+    // }
+
+    // Quick Quiz: Displaying the Array in Reverse order (for loop)
+    // System.out.println("Printing using for loop in reverse order");
+    // for (int i = marks.length - 1; i >= 0; i--) {
+    // System.out.println(marks[i]);
+    // }
+
+    // Quick Quiz: Displaying the Array (for-each loop)
+    // System.out.println("Printing using for-each loop");
+    // for (int element : marks) {
+    // System.out.println(element);
+    // }
+    public static void main(String[] args) {
+	
+		
+		
+		
+            String a = "Saurabh";
+    
+    
+            switch (a){
+                case "Shubham" : {
+                    System.out.println("You are going to become an Adult!");
+                    break;
+                }
+                case "Saurabh" :
+                System.out.println("You are going to join a Job!");
+                break;
+                case "Vishaka" :
+                System.out.println("You are going to get retired!");
+                break;
+                default : System.out.println("Enjoy Your life!");
+            }
+            System.out.println("Thanks for using my Java Code!");
+
+    }
+}
