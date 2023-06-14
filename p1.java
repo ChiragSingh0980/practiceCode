b66f6628388490fa5267612c90ddec9f36240341
ChiragSingh0980 <chiragsingh9725@gmail.com>
2023-06-14 13:14:15 +0530

    first commit

---
+++ p1.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        int i = 1;
+        while (i<=a) {
+            System.out.println("hello");
+            i=i+1;
+        }
+        sc.close();
+    }
+
+}
