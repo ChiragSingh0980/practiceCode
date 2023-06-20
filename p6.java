4ef5cfb91bbd6108ca85a7b32421a76c80b7dc78
ChiragSingh0980 <chiragsingh9725@gmail.com>
2023-06-20 13:14:15 +0530

    first commit

---
+++ p6.java
@@ -0,0 +1,169 @@
+class vit{
+    public int a;
+    private int b;
+    void cal(int x,int y){
+        a=x+3;
+        b=y;
+    }
+    
+
+// void print(){
+//     System.out.println("" +b);
+// }
+// }
+
+// }
+// interface A{
+//     void f();
+// }
+// interface B extends A{
+//     void g();
+
+// }
+// class C implements B{
+//     public void f(){
+//         System.out.println("vit");
+//     }
+//     public void g(){
+//         System.out.println("bhopal");
+//     }
+// }
+// interface Bus
+  
+// {
+  
+//   void move();
+  
+// }
+  
+// class ElectricBus implements Bus
+  
+// {
+  
+//   public void move()
+  
+//   {
+  
+//     System.out.println("No error");
+  
+//   }            
+  
+// }
+// class Sparrow{ }
+  
+// class BigSparrow extends Sparrow { }
+  
+// class Cat2
+  
+// {
+  
+//   Sparrow jumpingHeight(int weight)
+  
+//   {
+  
+//     System.out.println(40);
+  
+//     return new Sparrow();
+  
+//   }
+  
+// }
+  
+// class WildCat2 extends Cat2
+  
+// {
+  
+//   BigSparrow jumpingHeight(int weight)
+  
+//   {
+  
+//     System.out.println("50");
+  
+//     return new BigSparrow();
+  
+//   }
+  
+// }
+// class Animal{  
+//     String color="white";  
+//     }  
+//     class Dog extends Animal{  
+//     String color="black";  
+//     void printColor(){  
+//     System.out.println(color);//prints color of Dog class  
+//     System.out.println(super.color);//prints color of Animal class  
+//     }  
+//     }
+
+// public class p6 {
+
+    // final static short x = 2;
+
+    // public static int y = 0;
+
+    // public static void main(String[] args)
+
+    //  {
+        // Dog d=new Dog();  
+        // d.printColor();  
+        // }
+        // StringBuffer s1 = new StringBuffer("Complete");  
+        // s1.setCharAt(1,'p');  
+    //     s1.setCharAt(7,'Z');  
+    //     System.out.println(s1);  
+    //  }  
+//     }
+    //     WildCat2 wc = new WildCat2();
+  
+    //     wc.jumpingHeight(80);
+    // //     new ElectricBus().move();
+  
+    // }
+
+        // for (int z = 0; z < 2; z++)
+
+        // {
+
+        //     switch (z)
+
+        //     {
+
+        //         case x:
+        //             System.out.print("0 ");
+
+        //         case x - 1:
+        //             System.out.print("1 ");
+
+        //         case x - 2:
+        //             System.out.print("2 ");
+
+        //     }
+
+        // }
+
+        // int i = 1, j = 10;
+        // do {
+        // if (i > j) {
+        // break;
+        // }
+        // j--;
+        // } while (++i < 5);
+        // System.out.println("i = " + i + " and j = " + j);
+        // int g=6;
+        // System.out.println(++g*8);
+        // vit obj = new vit();
+        // obj.cal(12,9);
+        // System.out.println(obj.a);
+        // obj.print();
+        // C o = new C();
+        // o.f();
+        // o.g();
+        // int x = 4;
+        // System.out.print(++x * 8);
+
+
+        
+    
+
+
+
