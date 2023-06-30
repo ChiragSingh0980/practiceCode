0d211a67ecb2b1a9512306c74e7a71c7046b1132
ChiragSingh0980 <chiragsingh9725@gmail.com>
2023-06-30 13:14:15 +0530

    first commit

---
+++ Typecasting.java
@@ -0,0 +1,13 @@
+public class Typecasting {
+    public static void main(String[] args) {
+        byte x = 5;
+        float  y =x;
+        System.out.println(y);
+
+        double d =  5.55;
+        int a = (int)d;
+        System.out.println(d);
+        System.out.println(a);
+    }
+    
+}
